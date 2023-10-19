fecf701704d4591b77d0cee449bd1082db35a92b
Kauanaam <118370139+Kauanaam@users.noreply.github.com>
2023-10-19 10:58:52 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,8 @@
-document.querySelector('.tecla_pom')
+alert('essa pagina contem virus');
+
+function tocaSomPom() {document.querySelector('#som_tecla_pom').play()}
+
+document.querySelector('.tecla_pom').onclik 
 document.querySelector('.tecla_clap')
 document.querySelector('.tecla_tim')
 document.querySelector('.tecla_puff')
