156ae6f266176de425721d6cf45ed25e138a428f
Kauanaam <118370139+Kauanaam@users.noreply.github.com>
2023-10-26 10:58:05 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,15 +1,30 @@
 alert('essa pagina contem virus');
 
 function tocaSomPom() {document.querySelector('#som_tecla_pom').play()}
+document.querySelector('.tecla_pom').onclik=tocaSomPom;
 
-document.querySelector('.tecla_pom').onclik 
-document.querySelector('.tecla_clap')
-document.querySelector('.tecla_tim')
-document.querySelector('.tecla_puff')
-document.querySelector('.tecla_splash')
-document.querySelector('.tecla_toim')
-document.querySelector('.tecla_psh')
-document.querySelector('.tecla_tic')
-document.querySelector('.tecla_tom')
+function tocaSomClap() {document.querySelector('#som_tecla_clap').play()}
+document.querySelector('.tecla_clap').onclik=tocaSomClap;
 
-document.querySelector('#som_tecla_pom').play 
+function tocaSomTim() {document.querySelector('#som_tecla_tim').play()}
+document.querySelector('.tecla_tim').onclik=tocaSomTim;
+
+function tocaSomPuff() {document.querySelector('#som_tecla_puff').play()}
+document.querySelector('.tecla_puff').onclik=tocaSomPuff;
+
+function tocaSomSplash() {document.querySelector('#som_tecla_splash').play()}
+document.querySelector('.tecla_splash').onclik=tocaSomSplash;
+
+function tocaSomToim() {document.querySelector('#som_tecla_toim').play()}
+document.querySelector('.tecla_toim').onclik=tocaSomToim;
+
+function tocaSomPsh() {document.querySelector('#som_tecla_psh').play()}
+document.querySelector('.tecla_psh').onclik=tocaSomPsh;
+
+function tocaSomTic() {document.querySelector('#som_tecla_tic').play()}
+document.querySelector('.tecla_tic').onclik=tocaSomTic;
+
+function tocaSomTom() {document.querySelector('#som_tecla_tom').play()}
+document.querySelector('.tecla_tom').onclik=tocaSomTom;
+
+const listadetecla=document.querySelector('.tecla') 
